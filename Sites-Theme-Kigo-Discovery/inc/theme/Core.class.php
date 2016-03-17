19b5c06e60c86e96ba7ee82f1518b8a8c06b284b
Miquel Waller <miquel.waller@realpage.com>
2016-03-17 18:13:34 +0100

    Adding WeakMAp polyfill.

--- Sites-Theme-Kigo-Discovery/inc/theme/Core.class.php
+++ Sites-Theme-Kigo-Discovery/inc/theme/Core.class.php
@@ -60,18 +60,18 @@ class Core {
      */
     private function cleanUp(){
         add_action('after_setup_theme', function () {
-            remove_action('wp_head', 'wp_generator');                // #1
-            remove_action('wp_head', 'wlwmanifest_link');            // #2
-            remove_action('wp_head', 'rsd_link');                    // #3
-            remove_action('wp_head', 'wp_shortlink_wp_head');        // #4
+            remove_action('wp_head', 'wp_generator');                       // #1
+            remove_action('wp_head', 'wlwmanifest_link');                   // #2
+            remove_action('wp_head', 'rsd_link');                           // #3
+            remove_action('wp_head', 'wp_shortlink_wp_head');               // #4
 
-            remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);    // #5
+            remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);// #5
 
-            add_filter('the_generator', '__return_false');            // #6
-            add_filter('show_admin_bar','__return_false');            // #7
+            add_filter('the_generator', '__return_false');                  // #6
+            add_filter('show_admin_bar','__return_false');                  // #7
 
             remove_action( 'wp_head', 'print_emoji_detection_script', 7 );  // #8
-            remove_action( 'wp_print_styles', 'print_emoji_styles' );
+            remove_action( 'wp_print_styles', 'print_emoji_styles' );       // #9
         });
     }
 
