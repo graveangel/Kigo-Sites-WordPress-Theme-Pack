5bed2eea6ebd1d6445f2e2e00ca823260932dd75
Miquel Waller <miquel.waller@realpage.com>
2016-05-26 12:56:05 +0200

    Fixing kd-buckets to display all available + tweaking bapi.recursiveGet.

--- Sites-Theme-Kigo-Discovery/kd-common/js/main.js
+++ Sites-Theme-Kigo-Discovery/kd-common/js/main.js
@@ -111,24 +111,24 @@ var app = {
             });
         },
         recursiveGet: function(entity, callback, auxOptions){
-            var options = auxOptions || {};
+            var options = auxOptions || {pagesize: 20, waitForAll: false};
             BAPI.search(entity, options, function (sdata) {
-                var propPages = _.chunk(sdata.result, options.pagesize);
+                var pages = _.chunk(sdata.result, options.pagesize);
 
-                if(auxOptions.waitForAll == 1){
+                if(options.waitForAll == 1){
                     var all = [], iterations = 0;
-                    propPages.forEach(function(page, i){
+                    pages.forEach(function(page, i){
                         BAPI.get(page, entity, options, function (gdata) {
                             iterations++;
                             all = _.concat(all, gdata.result);
 
-                            if(iterations == (propPages.length)){ //Last iteration
+                            if(iterations == (pages.length)){ //Last iteration
                                 callback({result: all, textdata: BAPI.textdata});
                             }
                         });
                     });
                 }else{
-                    propPages.forEach(function(page, i){
+                    pages.forEach(function(page, i){
                         BAPI.get(page, entity, options, function (gdata) {
                             callback(gdata);
                         });
@@ -1461,10 +1461,10 @@ app.bapiModules.widgets.buckets = {
     selector: '.kd-buckets',
     init: function(){
         $(this.selector).each(function(index, ele){
-            app.bapiRender('searches', 'tmpl-searches-quickview', function(html){
-                ele.innerHTML = html;
-            }, {class: ele.dataset.class});
-
+            app.bapi.recursiveGet('searches',  function(data){
+                var finalData = Object.assign(data, {class: ele.dataset.class});
+                ele.innerHTML = app.bapi.render('tmpl-searches-quickview', finalData);
+            });
         });
     },
     cond: function cond() {
--- Sites-Theme-Kigo-Discovery/kd-common/js/widgets/kd-buckets.js
+++ Sites-Theme-Kigo-Discovery/kd-common/js/widgets/kd-buckets.js
@@ -2,10 +2,10 @@ app.bapiModules.widgets.buckets = {
     selector: '.kd-buckets',
     init: function(){
         $(this.selector).each(function(index, ele){
-            app.bapiRender('searches', 'tmpl-searches-quickview', function(html){
-                ele.innerHTML = html;
-            }, {class: ele.dataset.class});
-
+            app.bapi.recursiveGet('searches',  function(data){
+                var finalData = Object.assign(data, {class: ele.dataset.class});
+                ele.innerHTML = app.bapi.render('tmpl-searches-quickview', finalData);
+            });
         });
     },
     cond: function cond() {
