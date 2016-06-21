8cf3521bb32df9e402f45270abda1ad822bcf817
Wes Wilke <wes.wilke@realpage.com>
2016-06-21 08:35:04 -0500

    Testing

--- instaparent/page-templates/search-page.php
+++ instaparent/page-templates/search-page.php
@@ -176,48 +176,7 @@ if($using_new_stuff) {
 <article class="search-page">	
 <div class="row-fluid">
 
-  <?php if ( is_active_sidebar( 'insta-left-sidebar-search' ) ) : ?>
-      <aside class="span3">  	
-          <?php dynamic_sidebar( 'insta-left-sidebar-search' ); ?>
-      </aside>
-  <?php endif; ?>
-  
-  <?php if ( is_active_sidebar( 'insta-left-sidebar-search' ) && is_active_sidebar( 'insta-right-sidebar-search' )  ) : ?>
-  	<article class="span6">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <?php echo $output; ?>
-	<?php the_content(); ?>
-	<?php endwhile; else: ?>
-		<p><?php _e('Sorry, this page does not exist.'); ?></p>
-	<?php endif; ?>
-  </article>
-  <?php else: ?>
-  	<?php if ( !is_active_sidebar( 'insta-left-sidebar-search' ) && !is_active_sidebar( 'insta-right-sidebar-search' )  ) : ?>
-        <article class="span12">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php echo $output; ?>
-        <?php the_content(); ?>
-        <?php endwhile; else: ?>
-            <p><?php _e('Sorry, this page does not exist.'); ?></p>
-        <?php endif; ?>	
-        </article>
-    <?php else: ?>
-        <article class="span9">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php echo $output; ?>
-        <?php the_content(); ?>
-        <?php endwhile; else: ?>
-            <p><?php _e('Sorry, this page does not exist.'); ?></p>
-        <?php endif; ?>	
-        </article>
-    <?php endif; ?>
-  <?php endif; ?>
-  
-  <?php if ( is_active_sidebar( 'insta-right-sidebar-search' ) ) : ?>    
-  <aside class="span3">
-      <?php dynamic_sidebar( 'insta-right-sidebar-search' ); ?>
-  </aside>
-  <?php endif; ?>
+
   
 </div>
 
