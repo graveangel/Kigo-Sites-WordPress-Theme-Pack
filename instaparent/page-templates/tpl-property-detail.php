e74a7e119b2e3228be3d8b0bf65c70295e58f3fc
Wes Wilke <wes.wilke@realpage.com>
2016-07-29 13:03:04 -0500

    Remove timthumb

--- instaparent/page-templates/tpl-property-detail.php
+++ instaparent/page-templates/tpl-property-detail.php
@@ -56,7 +56,7 @@ if($data) {
 				<?php $imgCount = 1; foreach($data->Images as $img) { ?>
 					<li>
 						<div>
-							<img alt="<?php echo $img->Caption; ?>" title="Open Slideshow" <?php if($imgCount > 3) { echo 'src="/wp-content/plugins/bookt-api/img/loading-816x600.gif" data-'; } ?>src="/img.svc?src=http:<?php echo $img->OriginalURL; ?>&w=816&h=600&q=75&zc=2" />
+							<img alt="<?php echo $img->Caption; ?>" title="Open Slideshow" <?php if($imgCount > 3) { echo 'src="/wp-content/plugins/bookt-api/img/loading-816x600.gif" data-'; } ?>src="<?php echo $img->OriginalURL; ?>" style="width:100%; max-height:600px;" />
 							<?php if($img->Caption) { ?>
 							<p class="flex-caption">&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $img->Caption; ?></p>
 							<?php } ?>
