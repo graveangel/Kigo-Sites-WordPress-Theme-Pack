7a2d7af88551d2f4b973db92db51c157113327be
Wes Wilke <wes.wilke@realpage.com>
2016-08-16 16:16:53 -0500

    Update for missing image placeholder

--- instaparent/page-templates/tpl-property-detail.php
+++ instaparent/page-templates/tpl-property-detail.php
@@ -56,7 +56,7 @@ if($data) {
 				<?php $imgCount = 1; foreach($data->Images as $img) { ?>
 					<li>
 						<div>
-							<img alt="<?php echo $img->Caption; ?>" title="Open Slideshow" <?php if($imgCount > 3) { echo 'src="/wp-content/plugins/bookt-api/img/loading-816x600.gif" data-'; } ?>src="<?php echo $img->OriginalURL; ?>" style="width: auto; height: auto; max-width: 100%; max-height: 100%; margin: 0 auto; vertical-align: middle;" />
+							<img alt="<?php echo $img->Caption; ?>" title="Open Slideshow" <?php if($imgCount > 3) { echo 'src="/wp-content/plugins/bookt-api/img/loading-816x600.gif" data-'; } ?>src="<?php echo $img->OriginalURL; ?>" />
 							<?php if($img->Caption) { ?>
 							<p class="flex-caption">&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $img->Caption; ?></p>
 							<?php } ?>
