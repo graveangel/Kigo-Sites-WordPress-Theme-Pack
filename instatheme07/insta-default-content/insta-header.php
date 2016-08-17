dc17392de374c05e45eff58956bb53c607cc4806
Wes Wilke <wes.wilke@realpage.com>
2016-08-17 11:27:13 -0500

    Moves script from template files and into queue

--- instatheme07/insta-default-content/insta-header.php
+++ instatheme07/insta-default-content/insta-header.php
@@ -1,8 +1,3 @@
 <div class="top-links">
       <span class="currencyselector"></span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<span id="wishListLink"><a class="halflings white heart-empty" href="/rentalsearch/mylist"><i></i><span class="hidden-phone">{{textdata.My WishList}}</span></a></span>
 </div>
-<script type="text/javascript">
-	$(document).ready(function () {
-		BAPI.UI.createCurrencySelectorWidget('.currencyselector');
-	});
-</script>
