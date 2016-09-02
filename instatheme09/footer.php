11e96b937adc9585a9ce5a9e15ec2f593a024bb3
Wes Wilke <wes.wilke@realpage.com>
2016-09-02 12:49:32 -0500

    Theme updates for footer and search

--- instatheme09/footer.php
+++ instatheme09/footer.php
@@ -22,6 +22,8 @@ if(!$no_widgets){ $column_width = 12/$num_columns;}
     <!-- End page section -->
 </section>
 <!-- End pushdown section -->
+</article>
+<!-- End Main Content Wrapper -->
 <!-- Start Insta Footer Widget Area -->
 <footer id="insta-footer">
     <div class="container-fluid">
@@ -89,8 +91,7 @@ if(!$no_widgets){ $column_width = 12/$num_columns;}
 </div>
 <?php endif; ?>
 <!-- End Insta Bottom Fixed Widget Area -->
-</article>
-<!-- End Main Content Wrapper -->
+
 <?php wp_footer(); ?>
 <script type="text/javascript" src="<?php echo wp_make_link_relative(get_template_directory_uri()); ?>/insta-common/bootstrap/js/insta-common.js"></script>
 <script type="text/javascript">
