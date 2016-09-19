652efee95f4f9189f76b78833f505bf12e73dea6
bookt-jacob <jacob@bookt.com>
2016-09-19 15:39:50 -0400

    fixing load order (AGAIN!)

--- instaparent/widgets/widgets_include.php
+++ instaparent/widgets/widgets_include.php
@@ -64,15 +64,16 @@ require_once('WidgetBase.php');
 
 
 //....Bapi extension.......
-//add_action('plugins_loaded','load_xbapi');
-//function load_xbapi(){
+// MUST BE LOADED at 'plugins_loaded' hook or this will generate errors and fail to load BAPI/XBAPI EVERY TIME.  Please stop over-writing me.
+add_action('plugins_loaded','load_xbapi');
+function load_xbapi(){
 	if (class_exists('BAPI')) {
 		require_once('XBAPI.php');
 	}
-//}
 //Widgets base.............
 require_once('WidgetBase.php');
 //Property finders widget.......
 require_once ('SelectablePropertyFindersWidget/init.php');
 //Special Offers widget.......
 require_once ('SelectableSpecialOffersWidget/init.php');
+}
